07e71a3e8aa7a1abdee15eaa9b6e07ec03b0365f
basket.baza@gmail.com <basket.baza@gmail.com>
2020-03-12 16:58:23 +0300

    Game on kotlin init

--- src/Game.kt
+++ src/Game.kt
@@ -1,10 +1,31 @@
 fun main() {
     val name = "Madrigal"
-    var healthPoints = 100
+    var healthPoints = 89
+    var isBlessed: Boolean = true
+    val isImmortal = false
+    // Аура
+    val auraVisible = isBlessed && healthPoints > 50 || isImmortal
+//    if (isBlessed && healthPoints > 50 || isImmortal) {
+   if (auraVisible) {
+        println("GREEN")
+    } else {
+        println("NONE")
+    }
 
-    if (healthPoints==100){
+    if (healthPoints == 100) {
         println(name + " is in excellent condition!")
+    } else if (healthPoints >= 90) {
+        println(name + " has a few scratches.")
+    } else if (healthPoints >= 75) {
+        if (isBlessed) {
+            println(name + " has some minor wounds but is healing quite quickly!")
+        } else {
+            println(name + " has some minor wounds.")
+        }
+    } else if (healthPoints >= 15) {
+        println(name + " looks pretty hurt.")
     } else {
         println(name + " is in awful condition!")
     }
+
 }

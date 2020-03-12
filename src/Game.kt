2968b9e686f886343967812c56a2685de4ca0d42
basket.baza@gmail.com <basket.baza@gmail.com>
2020-03-12 16:35:02 +0300

    Initial commit

---
+++ src/Game.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val name = "Madrigal"
+    var healthPoints = 100
+
+    if (healthPoints==100){
+        println(name + " is in excellent condition!")
+    } else {
+        println(name + " is in awful condition!")
+    }
+}

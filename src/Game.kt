1ae03885ed7e21e4fad15f84088becdc7417db41
basket.baza@gmail.com <basket.baza@gmail.com>
2020-03-13 08:18:41 +0300

    Условное выражение, используем вместо условного оператора

--- src/Game.kt
+++ src/Game.kt
@@ -1,6 +1,7 @@
 fun main() {
     val name = "Madrigal"
     var healthPoints = 89
+
     var isBlessed: Boolean = true
     val isImmortal = false
     // Аура
@@ -12,20 +13,29 @@ fun main() {
         println("NONE")
     }
 
-    if (healthPoints == 100) {
-        println(name + " is in excellent condition!")
+    val healthStatus:String=if (healthPoints == 100) {
+//        println(name + )
+        " is in excellent condition!"
     } else if (healthPoints >= 90) {
-        println(name + " has a few scratches.")
+//        println(name + " has a few scratches.")
+        " has a few scratches."
     } else if (healthPoints >= 75) {
         if (isBlessed) {
-            println(name + " has some minor wounds but is healing quite quickly!")
+
+//            println(name + " has some minor wounds but is healing quite quickly!")
+            " has some minor wounds but is healing quite quickly!"
         } else {
-            println(name + " has some minor wounds.")
+//            println(name + " has some minor wounds.")
+            " has some minor wounds."
         }
     } else if (healthPoints >= 15) {
-        println(name + " looks pretty hurt.")
+//        println(name + " looks pretty hurt.")
+        " looks pretty hurt."
     } else {
-        println(name + " is in awful condition!")
+//        println(name + " looks pretty hurt.")
+        " looks pretty hurt."
     }
+
+    println("Состояние игрока $name: "+healthStatus)
 
 }
